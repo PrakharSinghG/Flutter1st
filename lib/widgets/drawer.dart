6360260f4bb92cff2b9e4cad4428c00c17327267
Prakhar Singh <prakharsingh2001@hmail.com>
2022-04-23 20:00:00 +0530

    Day 9 Completed

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Mydrawer extends StatelessWidget {
+  const Mydrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const imageURL =
+        "https://socialsfrag.com/wp-content/uploads/2022/04/Why-Gojo-Satoru-Covers-His-Eyes-And-Can-He-See-Through-His-Blindfold_-758x426.jpg";
+    return Drawer(
+      child: Container(
+        color: Colors.purple[800],
+        child: ListView(
+          padding: EdgeInsets.zero,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                decoration: BoxDecoration(color: Colors.purple[800]),
+                accountName: Text("Prakhar"),
+                accountEmail: Text("prakharsingh2001@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageURL),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
