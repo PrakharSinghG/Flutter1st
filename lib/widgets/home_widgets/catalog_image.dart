77ce03cde494a0739a0153b1d8df6c5e0b69040e
Prakhar Singh <prakharsingh2001@hmail.com>
2022-05-12 00:06:38 +0530

    day 18 completed

---
+++ lib/widgets/home_widgets/catalog_image.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../../widgets/themes.dart';
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .p8
+        .rounded
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .wh40(context);
+  }
+}
