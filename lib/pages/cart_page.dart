3690b862d2cf81c10d9ae9e4a094882bed524395
Prakhar Singh <prakharsingh2001@hmail.com>
2022-05-18 19:53:34 +0530

    day 22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -14,6 +14,59 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          const _CartList().p32().expand(),
+          const Divider(),
+          const _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: "Buy".text.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: const Icon(Icons.done),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.remove_circle_outline),
+        ),
+        title: "Item 1".text.make(),
+      ),
     );
   }
 }
